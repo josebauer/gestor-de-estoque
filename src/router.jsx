af6ccaa0afaeaf0a1bae1c2b5e9a2a1ab4e5f431
José Bauer <jose.h.bauer03@gmail.com>
2023-11-07 21:31:42 -0300

    Cria arquivos das páginas e installa o react-bootstrap

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import RootLayout from "./pages/RootLayout";
+import ListItems from "./pages/items/ListItems";
+import ShowItem from "./pages/items/ShowItem";
+import UpdateItem from "./pages/items/UpdateItem";
+import ItemsLayout from "./pages/items/ItemsLayout";
 
 const router = createBrowserRouter([{
   path: '/',
@@ -9,9 +15,9 @@ const router = createBrowserRouter([{
       path: 'items',
       element: <ItemsLayout />,
       children: [
-        { index: true, element: <ListItems /> }
-        { path: 'new', element: <CreateItem /> }
-        { path: ':id', element: <ShowItem /> }
+        { index: true, element: <ListItems /> },
+        { path: 'new', element: <CreateItem /> },
+        { path: ':id', element: <ShowItem /> },
         { path: ':id/update', element: <UpdateItem /> }
       ]
     }
