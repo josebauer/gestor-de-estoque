5809d5d5b75134ef31cfd50c9187e28f09f79565
José Bauer <jose.h.bauer03@gmail.com>
2023-11-09 14:16:22 -0300

    Cria componente de contexto StockContext

---
+++ src/contexts/StockContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react";
+
+export const StockContext = createContext({})
+
+export function StockContextProvider({ children }) {
+  const [items, setItems] = useState(() => {
+    const storedItems = localStorage.getItem('stock-manager')
+
+    if (!storedItems) return []
+
+    const items = JSON.parse(storedItems)
+
+    items.forEach((item) => {
+      item.createdAt = new Date(item.createdAt)
+      item.updatedAt = new Date(item.updatedAt)
+    });
+    return items
+  })
+
+  const addItem = (item) => {
+    setItems(currentState => {
+      const updatedItems = [item, ...currentState]
+      localStorage.setItem('stock-manager', JSON.stringify(updatedItems))
+      return updatedItems
+    })
+  }
+
+  const stock = {
+    items, 
+    addItem
+  }
+
+  return (
+    <StockContext.Provider value={stock}>
+      {children}
+    </StockContext.Provider>
+  )
+}
